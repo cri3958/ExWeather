9705428cfa5289bb75841e53669db4517ff9f67a
cri3958 <leehojin0207@naver.com>
2021-09-15 21:49:12 +0900

    for(i=0;i<items.size;i++)  if(items.category==T1H)~~

--- app/src/main/java/com/hojin/ex_weather/WeatherInterface.kt
+++ app/src/main/java/com/hojin/ex_weather/WeatherInterface.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherInterface {
-    @GET("getVilageFcst?serviceKey=" +
+    @GET("getUltraSrtFcst?serviceKey=" +
             "PopaVGfopxS%2FhgXo2zqOaRTtWU3j4ADDJo%2BRwb9rNpPV7%2FRUPlvowNT7PFdIkauBn2fV0m1LbEJm84ZXnvAW5w%3D%3D")
     fun GetWeather(
         @Query("dataType") data_type : String,
